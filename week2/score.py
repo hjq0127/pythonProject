0268c0b8f150451092d167f37feee1637c381357
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-11-14 16:25:48 +0800

    week2

--- week2/score.py
+++ week2/score.py
@@ -1,9 +1,12 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+
+
+    score = float(input("Enter score: "))
+       if score < 0 or score > 100:
+          print("Invalid score")
+        elif score >= 90:
+          print("Excellent")
+        elif score >= 50:
+          print("Passable")
+        else:
+          print("Bad")
