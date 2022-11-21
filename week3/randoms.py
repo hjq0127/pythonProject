3e98b6be7267114373edef61212d374d6cbbb2ae
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-11-21 15:04:06 +0800

    week3

--- week3/randoms.py
+++ week3/randoms.py
@@ -1,17 +1,19 @@
-# What did you see on line 1?
-# 7
-# What was the smallest number you could have seen, what was the largest?
-# Smallest is 5, largest is 20
-# What did you see on line 2?
-# 3
-# What was the smallest number you could have seen, what was the largest?
-# Smallest is 3, largest is 9
-# Could line 2 have produced a 4?
-# Cannot
-# What did you see on line 3?
-# 3.8856197973174544
-# What was the smallest number you could have seen, what was the largest?
-# Smallest is 2.5, largest is 5.5
+"""
+ What did you see on line 1?
+ 7
+ What was the smallest number you could have seen, what was the largest?
+ Smallest is 5, largest is 20
+ What did you see on line 2?
+ 3
+ What was the smallest number you could have seen, what was the largest?
+ Smallest is 3, largest is 9
+ Could line 2 have produced a 4?
+ Cannot
+ What did you see on line 3?
+ 3.8856197973174544
+ What was the smallest number you could have seen, what was the largest?
+ Smallest is 2.5, largest is 5.5
+"""
 import random
 
 print(random.randint(5, 20))  # line 1
