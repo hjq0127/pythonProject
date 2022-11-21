e0da9c6fdc2c8768ec53818d0561b0e949f17903
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-11-21 16:53:29 +0800

    week3

--- week3/exceptions_demo.py
+++ week3/exceptions_demo.py
@@ -8,16 +8,17 @@ numerator and denominator = 0
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 
 """
-
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
 except ZeroDivisionError:
-
     print("Cannot divide by zero!")
+
 
 print("Finished.")
