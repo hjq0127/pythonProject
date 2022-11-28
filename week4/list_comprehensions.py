c8929960672fbc3874bfc316039b9d88d372a561
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-11-28 16:28:21 +0800

    week4

--- week4/list_comprehensions.py
+++ week4/list_comprehensions.py
@@ -51,3 +51,6 @@ print(big_numbers)
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+Name_more_than_11 = list(filter(lambda s: isinstance(s, str) and len(s) >= 11, full_names))
+print(Name_more_than_11)
+
