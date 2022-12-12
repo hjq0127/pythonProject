31cb8fdee29a86869d1579a84efe44080b71b2a4
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-12-12 19:07:07 +0800

    week6

--- week6/languages.py
+++ week6/languages.py
@@ -4,7 +4,7 @@ from programming_language import ProgrammingLanguage
 def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("visual Basic", "Static", False, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
     languages = [ruby, python, visual_basic]
     print("The dynamically typed languages are:")
