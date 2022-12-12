8a4c619c89d212da0af9fc2df152477044e83a7a
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-12-12 19:10:18 +0800

    week6

--- week6/used_cars.py
+++ week6/used_cars.py
@@ -1,7 +1,7 @@
 """
 CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
-This is why we name our folders with no spaces or capitals, as valid module names.
+This is why we name  our folders with no spaces or capitals, as valid module names.
 """
 
 from car import Car
