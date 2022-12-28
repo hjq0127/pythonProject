ce8165d0557634133166f0c43586031f8d1503f8
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2022-12-28 17:27:07 +0800

    week8

---
+++ week8/dynamic_labels.py
@@ -0,0 +1,56 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+NEW_COLOUR = (0, 0, 0.34, 1)  # RGBA for red
+ALTERNATIVE_COLOUR = (0, 0, 0.67, 1)  # RGBA for magenta
+
+
+class DynamicWidgetsApp(App):
+    """Main program - Kivy app to demo dynamic widget creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        # basic data (model) example - dictionary of names: phone numbers
+        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """Create buttons from data and add them to the GUI."""
+        for name in self.name_to_phone:
+            # create a button for each data entry, specifying the text
+            temp_button = Button(text=name)
+            temp_button.bind(on_press=self.press_entry)
+            # set the button's background colour
+            temp_button.background_color = NEW_COLOUR
+            # add the button to the "entries_box" layout widget
+            self.root.ids.entries_box.add_widget(temp_button)
+
+    def press_entry(self, instance):
+        """Handle pressing entry buttons."""
+        # get name (dictionary key) from the text of Button we clicked on
+        name = instance.text
+        # change the button's background colour
+        instance.background_color = ALTERNATIVE_COLOUR
+        # update status text
+        self.status_text = f"{name}'s number is {self.name_to_phone[name]}"
+
+    def clear_all(self):
+        """Clear all widgets that are children of the "entries_box" layout widget."""
+        self.root.ids.entries_box.clear_widgets()
+
+    def add_widget(self, temp_button):
+        self.root.ids.entries_box.add_widget(temp_button)
+        temp_button.bind(on_release=self.press_entry)
+
+
+DynamicWidgetsApp().run()
