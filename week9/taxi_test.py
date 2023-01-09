d15d47f9c66bc796ac78041a6d11f697ed65b7bc
hjq0127 <jiaqin.hong@my.jcu.edu.au>
2023-01-09 15:10:38 +0800

    week9

---
+++ week9/taxi_test.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+Test taxi
+"""
+
+from week9.car import Car
+
+
+def main():
+    """Test Taxi class."""
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
